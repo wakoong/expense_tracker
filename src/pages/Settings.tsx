a6d859fe08c3ae4c6c6550cfcef2cbe91168241c
Woosik Koong <woosik.koong@gmail.com>
2020-04-10 14:48:36 -0700

    Settings page

---
+++ src/pages/Settings.tsx
@@ -0,0 +1,35 @@
+// react imports
+import React from 'react';
+import { Link } from 'react-router-dom';
+// imports
+import Header from '../components/Header';
+import SidePanel from '../components/Panels/SidePanel';
+import Account from '../components/Settings/Account';
+// 3rd-party imports
+import styled from 'styled-components';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+
+const Layout = styled.div`
+  height: 100vh;
+  display: grid;
+  grid-template-rows: 50px auto;
+`;
+
+export function Settings() {
+  return (
+    <Layout>
+      <Header title="Settings" link="">
+        <Link to="/">
+          <ExitToAppIcon />
+        </Link>
+      </Header>
+      <div>
+        <SidePanel tabs={['Account', 'Fixed Cost', 'Variable Cost']}>
+          <Account />
+          <div>1</div>
+          <div>2</div>
+        </SidePanel>
+      </div>
+    </Layout>
+  );
+}
