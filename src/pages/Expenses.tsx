20b558019d5433daa893399cbc7594a86859e4c3
Woosik Koong <woosik.koong@gmail.com>
2020-05-11 23:35:30 -0700

    Apply Nintendo theme for expense page

--- src/pages/Expenses.tsx
+++ src/pages/Expenses.tsx
@@ -7,61 +7,175 @@ import { GlobalStyle } from '../utils/Global';
 // 3rd-party imports
 import styled from 'styled-components';
 
-const FlexCenter = styled.div`
+const Wrapper = styled.div`
+  position: relative;
+  display: grid;
+  grid-template-rows: 200px 1fr;
+  height: calc(100% - 8px);
+  padding: 0px !important;
+`;
+
+const Header = styled.div`
+  position: relative;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
+
+  .mode-btn {
+    position: absolute;
+    top: 0;
+    right: 0;
+  }
+
+  .header_title {
+    font-size: 1.5em;
+  }
+
+  .content {
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    font-size: 26px;
+    width: 75%;
+  }
 `;
 
-const Layout = styled(FlexCenter)`
-  height: 100%;
-  position: relative;
-  overflow: hidden;
-  box-sizing: border-box;
+const Summary = styled.div`
+  position: absolute;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  text-align: center;
+  bottom: -40px;
+  height: 80px;
+  width: 300px;
+  z-index: 10;
+  background-color: white;
+  padding: 0px !important;
+
+  .cost-block {
+    display: grid;
+    grid-template-rows: 30px 1fr;
+    font-size: 1em;
+
+    .cost_title,
+    .cost {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+    .cost_title.fc {
+      background-color: #92cc41;
+    }
+    .cost_title.vc {
+      background-color: #f7d51d;
+    }
+    .cost {
+      color: #21252a;
+    }
+  }
 `;
 
-const FixedCostWrapper = styled(FlexCenter)`
-  width: calc(100vw - 400px);
-  height: 30vh;
-  background-color: ${defaultTheme.status.warningColor};
-`;
+const Body = styled.div`
+  width: 100%;
+  padding: 0px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 
-const VariableCostWrapper = styled(FlexCenter)`
-  width: calc(100vw - 400px);
-  height: 70vh;
-  background-color: ${defaultTheme.status.errorColor};
+  .body_content {
+    margin-top: 40px;
+    width: 90%;
+  }
 `;
-
-const fixedCost = [
-  { label: 'rent', expense: 1505 },
-  { label: 'insurance', expense: 60 },
-  { label: 'phone', expense: 100 },
-];
 
 export default function Expenses() {
+  const [mode, setMode] = useState(true);
+  console.log({ mode });
   return (
-    <Layout>
-      <Calculator />
-      <div>
-        <FixedCostWrapper>
-          {fixedCost.map((cost) => (
-            <CostBox
-              key={cost.label}
-              label={cost.label}
-              expense={cost.expense}
-            />
-          ))}
-        </FixedCostWrapper>
-        <VariableCostWrapper>
-          {fixedCost.map((cost) => (
-            <CostBox
-              key={cost.label}
-              label={cost.label}
-              expense={cost.expense}
-            />
-          ))}
-        </VariableCostWrapper>
-      </div>
-    </Layout>
+    <Wrapper className={`nes-container is-rounded`}>
+      <Header className={`nes-container ${mode ? '' : 'is-dark'}`}>
+        <button
+          type="button"
+          className={`nes-btn mode-btn ${mode ? 'is-warning' : 'disabled'}`}
+          onClick={() => setMode(!mode)}
+        >
+          {mode ? '🌞' : '🌚'}
+        </button>
+        <span className="nes-text is-success header_title">
+          Available Budget
+        </span>
+        <div className="content">
+          <i className="snes-jp-logo"></i>
+          <div>$ 2,000</div>
+        </div>
+        <Summary className={`nes-container`}>
+          <div className="cost-block">
+            <div className="cost_title fc">Fixed Cost</div>
+            <div className="cost">$1,000</div>
+          </div>
+          <div className="cost-block">
+            <div className="cost_title vc">Variable Cost</div>
+            <div className="cost">$2,000</div>
+          </div>
+        </Summary>
+      </Header>
+      <Body className={`nes-container ${mode ? '' : 'is-dark'}`}>
+        <div className="body_content">
+          <progress className="nes-progress" value="90" max="100"></progress>
+          <progress
+            className="nes-progress is-primary"
+            value="80"
+            max="100"
+          ></progress>
+          <progress
+            className="nes-progress is-success"
+            value="50"
+            max="100"
+          ></progress>
+          <progress
+            className="nes-progress is-warning"
+            value="30"
+            max="100"
+          ></progress>
+          <progress
+            className="nes-progress is-error"
+            value="10"
+            max="100"
+          ></progress>
+          <progress
+            className="nes-progress is-pattern"
+            value="50"
+            max="100"
+          ></progress>
+          <progress className="nes-progress" value="90" max="100"></progress>
+          <progress
+            className="nes-progress is-primary"
+            value="80"
+            max="100"
+          ></progress>
+          <progress
+            className="nes-progress is-success"
+            value="50"
+            max="100"
+          ></progress>
+          <progress
+            className="nes-progress is-warning"
+            value="30"
+            max="100"
+          ></progress>
+          <progress
+            className="nes-progress is-error"
+            value="10"
+            max="100"
+          ></progress>
+          <progress
+            className="nes-progress is-pattern"
+            value="50"
+            max="100"
+          ></progress>
+        </div>
+      </Body>
+    </Wrapper>
   );
 }
